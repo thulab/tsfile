a3d0a0b850aa01af6deeca626a1146574c3f8290
xingtanzjr <xingtanzjr@163.com>
2017-04-04 11:23:43 +0800

    prepare for rebuild structures

--- tsfile-impl-parent/tsfile-common/src/main/java/com/corp/delta/tsfile/common/constant/SystemConstant.java
+++
@@ -1,5 +0,0 @@
-package com.corp.delta.tsfile.common.constant;
-
-public class SystemConstant {
-    public static final String TSFILE_HOME = "TSFILE_HOME";
-}
--- tsfile-impl-parent/tsfile-timeseries/src/main/java/com/corp/delta/tsfile/read/query/QueryType.java
+++
@@ -1,5 +0,0 @@
-package com.corp.delta.tsfile.read.query;
-
-public enum QueryType {
-	QUERY_WITHOUT_FILTER,SELECT_ONE_COL_WITH_FILTER,CROSS_QUERY
-}
