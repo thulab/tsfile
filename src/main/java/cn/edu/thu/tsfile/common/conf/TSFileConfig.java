8a4f2cba6ef657fe2741d6a36daac2eaeac47e1a
XuYi <xuyithuss@126.com>
2017-05-11 18:44:45 +0800

    remove overflow config

--- src/main/java/cn/edu/thu/tsfile/common/conf/TSFileConfig.java
+++ src/main/java/cn/edu/thu/tsfile/common/conf/TSFileConfig.java
@@ -111,46 +111,6 @@ public class TSFileConfig {
     public int defaultDFTMainFreqNum = 2;
 
     public double dftSatisfyRate = 0.1;
-    /**
-     * the maximum number of writing instances existing in same time.
-     */
-    public int writeInstanceThreshold = 5;
-
-    /**
-     * data directory of Overflow data
-     */
-    public String overflowDataDir = "src/main/resources/data/overflow";
-    /**
-     * data directory of fileNode data
-     */
-    public String FileNodeDir = "src/main/resources/data/digest";
-    /**
-     * data directory of bufferWrite data
-     */
-    public String BufferWriteDir = "src/main/resources/data/delta";
-
-    public String metadataDir = "src/main/resources/metadata";
-
-    public String derbyHome = "src/main/resources/derby";
-
-    /**
-     * maximum concurrent thread number for merging overflow
-     */
-    public int mergeConcurrentThreadNum = 10;
-    /**
-     * the maximum number of concurrent file node instances
-     */
-    public int maxFileNodeNum = 1000;
-    /**
-     * the maximum number of concurrent overflow instances
-     */
-    public int maxOverflowNodeNum = 100;
-    /**
-     * the maximum number of concurrent buffer write instances
-     */
-    public int maxBufferWriteNodeNum = 50;
-    public int defaultFetchSize = 5000;
-    public String writeLogPath = "src/main/resources/writeLog.log";
 
     public final int RLE_MIN_REPEATED_NUM = 8;
     public final int RLE_MAX_REPEATED_NUM = 0x7FFFFF;
@@ -159,14 +119,6 @@ public class TSFileConfig {
     public final int BITMAP_BITWIDTH = 1;
 
     public TSFileConfig() {
-	String home = System.getProperty(SystemConstant.TSFILE_HOME, "");
-	if (!home.equals("")) {
-	    overflowDataDir = home + "/data/overflow";
-	    FileNodeDir = home + "/data/digest";
-	    BufferWriteDir = home + "/data/delta";
-	    writeLogPath = home + "/data/writeLog.log";
-	    metadataDir = home + "/data/metadata";
-	    derbyHome = home + "/data/derby";
-	}
+
     }
 }
