a7e7491ce6ae43e21153d43d4b5dea78b6429b80
xiangdong huang <sainthxd@gmail.com>
2017-10-17 14:28:26 +0800

    remove unuseful imports and code format

--- src/main/java/cn/edu/tsinghua/tsfile/timeseries/read/LocalFileInput.java
+++ src/main/java/cn/edu/tsinghua/tsfile/timeseries/read/LocalFileInput.java
@@ -7,8 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-import org.apache.commons.io.IOUtils;
-
 /**
  * File-read interface for local file.
  *
@@ -16,53 +14,53 @@ import org.apache.commons.io.IOUtils;
  */
 public class LocalFileInput implements TSRandomAccessFileReader {
 
-    private RandomAccessFile raf;
+  private RandomAccessFile raf;
 
-    public LocalFileInput(String path) throws FileNotFoundException {
-        this.raf = new RandomAccessFile(path, "r");
-    }
+  public LocalFileInput(String path) throws FileNotFoundException {
+    this.raf = new RandomAccessFile(path, "r");
+  }
 
-    @Override
-    public void seek(long offset) throws IOException {
-        this.raf.seek(offset);
-    }
+  @Override
+  public void seek(long offset) throws IOException {
+    this.raf.seek(offset);
+  }
 
-    public int read() throws IOException {
-        return raf.read();
-    }
+  public int read() throws IOException {
+    return raf.read();
+  }
 
-    public int read(byte[] b, int off, int len) throws IOException {
-    		int end=len+off;
-    		int get=1;
-    		int total=0;
-    		for(int i=off;i<end; i+=get) {
-    			get=raf.read(b, i, end-i);
-    			if(get>0)
-    				total+=get;
-    			else
-    				break;
-    		}
-        return total;
+  public int read(byte[] b, int off, int len) throws IOException {
+    int end = len + off;
+    int get = 1;
+    int total = 0;
+    for (int i = off; i < end; i += get) {
+      get = raf.read(b, i, end - i);
+      if (get > 0)
+        total += get;
+      else
+        break;
     }
+    return total;
+  }
 
-    public long length() throws IOException {
-        return raf.length();
-    }
+  public long length() throws IOException {
+    return raf.length();
+  }
 
-    @Override
-    public int readInt() throws IOException {
-        return raf.readInt();
-    }
+  @Override
+  public int readInt() throws IOException {
+    return raf.readInt();
+  }
 
-    /**
-     * use {@code FileStreamManager} to manage all LocalFileInput
-     */
-    public void closeFromManager() {
-        FileStreamManager.getInstance().close(this);
-    }
+  /**
+   * use {@code FileStreamManager} to manage all LocalFileInput
+   */
+  public void closeFromManager() {
+    FileStreamManager.getInstance().close(this);
+  }
 
-    @Override
-    public void close() throws IOException {
-        raf.close();
-    }
+  @Override
+  public void close() throws IOException {
+    raf.close();
+  }
 }
