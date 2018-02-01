8a20462665597bb92b3b40b0a9047db42a635420
liukun06 <451564319@qq.com>
2018-02-01 17:56:33 +0800

    add error log in flush timeseries data

---
+++ src/main/java/cn/edu/tsinghua/tsfile/timeseries/readV2/datatype/Test.java
@@ -0,0 +1,10 @@
+package cn.edu.tsinghua.tsfile.timeseries.readV2.datatype;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new TsPrimitiveType.TsBoolean(false);
+	}
+
+}
