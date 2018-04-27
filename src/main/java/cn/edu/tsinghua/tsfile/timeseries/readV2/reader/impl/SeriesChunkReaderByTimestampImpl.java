c79f77110d32b013619f2616d484bccb6a8c956f
jt <jt2594838@163.com>
2018-04-27 12:55:52 +0800

    add maxTimbstoneTime in series chunk reader and series reader

--- src/main/java/cn/edu/tsinghua/tsfile/timeseries/readV2/reader/impl/SeriesChunkReaderByTimestampImpl.java
+++ src/main/java/cn/edu/tsinghua/tsfile/timeseries/readV2/reader/impl/SeriesChunkReaderByTimestampImpl.java
@@ -22,7 +22,7 @@ public class SeriesChunkReaderByTimestampImpl extends SeriesChunkReader {
     public boolean pageSatisfied(PageHeader pageHeader) {
         long maxTimestamp = pageHeader.data_page_header.max_timestamp;
         //If minTimestamp > currentTimestamp, this page should NOT be skipped
-        if (maxTimestamp < currentTimestamp) {
+        if (maxTimestamp < currentTimestamp || maxTimestamp < getMaxTombstoneTime()) {
             return false;
         }
         return true;
@@ -30,7 +30,7 @@ public class SeriesChunkReaderByTimestampImpl extends SeriesChunkReader {
 
     @Override
     public boolean timeValuePairSatisfied(TimeValuePair timeValuePair) {
-        return true;
+        return timeValuePair.getTimestamp() > getMaxTombstoneTime();
     }
 
     public void setCurrentTimestamp(long currentTimestamp) {
