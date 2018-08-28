c7a5669c46d05f80f6ae7b07e9db69919ed013d3
mdf369 <952945925@qq.com>
2018-08-28 15:41:54 +0800

    update UT

--- src/test/java/cn/edu/tsinghua/tsfile/tool/TsFileAnalyzerTest.java
+++ src/test/java/cn/edu/tsinghua/tsfile/tool/TsFileAnalyzerTest.java
@@ -1,11 +1,9 @@
 package cn.edu.tsinghua.tsfile.tool;
 
-import org.junit.Test;
-
 import java.io.IOException;
 
 public class TsFileAnalyzerTest {
-    
+
     public static void main(String[] args) throws IOException {
         String inputFilePath = "test.ts";
         String outputFilePath = "report.txt";
